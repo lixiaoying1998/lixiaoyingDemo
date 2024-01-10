2f2086d55ce215e96df4d9e5d9baa7550d327c39
lixiaoying1998 <15751453729@163.com>
2024-01-10 10:03:10 +0800

    init

---
+++ react-demo/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
